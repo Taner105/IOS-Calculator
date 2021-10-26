8aa5b1a49c4e807945eae20663e97de6f86fb746
Taner105 <taneraydogan59@gmail.com>
2021-10-26 17:27:56 +0300

    added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let numbers = document.getElementsByClassName("num");
 let result1 = document.querySelector(".resultBay");
 let signs = document.getElementsByClassName("sign");
 let operators = document.getElementsByClassName("operator");
+let equals = document.getElementById("equals");
+
+// ---------------NUMBERS-----------
 
 for (var i = 0; i < numbers.length; i++){
     numbers[i].addEventListener("click", yazdir)
@@ -11,6 +14,7 @@ function yazdir(){
 }
 
 
+// ---------------SİGNS--------------
 
 for (var i = 0; i < signs.length; i++){
     signs[i].addEventListener("click", yazdir)
@@ -18,16 +22,19 @@ for (var i = 0; i < signs.length; i++){
 function yazdir(){
     result1.innerHTML += this.value 
 }
-// let okay = document.getElementById("ok");
-// okay.addEventListener("click", sil);
-// function sil(){
-//    ok.remove().innerHTML = this.value;
-// }
-
+// --------------OPERATORS-------------
 
 for (var i = 0; i < operators.length; i++){
+    if (i != 4){
     operators[i].addEventListener("click", yazdir)
+    }
 }
 function yazdir(){
     result1.innerHTML += this.value 
+}
+
+equals.addEventListener("click", eşittirSonuc);
+
+function eşittirSonuc() {
+    result1.innerHTML = eval(result1.class);
 }
